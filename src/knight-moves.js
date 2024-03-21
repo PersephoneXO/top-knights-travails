e203f88b5ba8da5675fc886baa4cd5da9da929cb
Persephone <its.persephone.xo@gmail.com>
2024-03-21 15:08:05 -0400

    movesQueue works, just need to figure out how to find the shortest path

--- src/knight-moves.js
+++ src/knight-moves.js
@@ -1,5 +1,5 @@
 //main knightMoves() function
- export function knightMoves(start,end){
+ export function knightMoves(start,target){
 
  }
 
@@ -42,3 +42,30 @@
     }
     return allMoves;
  }
+
+ //function that makes its way through a queue and analyzes the input
+ function movesQueue(start,target){
+    let queue=[start];
+    let visited=[];
+    let reachedTarget=false;
+    while(!reachedTarget){
+        let currentIndex=queue.shift();
+        visited.push(currentIndex);
+
+        //check if the current index is the target
+        if(currentIndex[0]==target[0]&&currentIndex[1]==target[1]){
+            reachedTarget=true;
+        }
+
+        //get possible moves from the current index
+        let indexMoves=possibleMoves(currentIndex);
+        for(const each of indexMoves){
+            if(!visited.some(([x,y])=>x===each[0]&&y===each[1])){
+                queue.push(each);
+            }
+        }
+    }
+    return visited;
+ }
+
+ console.log(movesQueue([0,0],[3,3]));
