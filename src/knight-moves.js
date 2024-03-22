4c817c3d24946134aaa95f996b6559c3afa19321
Persephone <its.persephone.xo@gmail.com>
2024-03-22 12:55:24 -0400

    knightMoves() works as intended, project finished

--- src/knight-moves.js
+++ src/knight-moves.js
@@ -1,6 +1,12 @@
 //main knightMoves() function
 export function knightMoves(start,target){
+    let visitedIndexes=movesQueue(start,target);
+    let theShortestPath=shortestPath(visitedIndexes,start,target);
 
+    console.log(`You made it in ${theShortestPath.length-1} moves! Here's your path:`)
+    for(const each of theShortestPath){
+        console.log(each);
+    }
 }
 
 //helper functions
@@ -48,7 +54,7 @@ function movesQueue(start,target){
     let queue=[start];
     let visited=[];
     let reachedTarget=false;
-    while(!reachedTarget){
+    while(!reachedTarget && queue.length>0){
         let currentIndex=queue.shift();
         visited.push(currentIndex);
 
@@ -65,33 +71,36 @@ function movesQueue(start,target){
             }
         }
     }
-    return {visited,start,target};
+    return visited;
  }
 
  //function that sorts through an array of the visited indexes and finds the shortest path
 function shortestPath(visitedIndexes,start,target){
-    let path=[target];
-    let currentIndex=target;
-
-    while(!(currentIndex[0]===start[0]&&currentIndex[1]===start[1])){
-        let currentX=currentIndex[0];
-        let currentY=currentIndex[1];
-
-        for(const [parentX,parentY] of visitedIndexes){
-            if(
-                (parentX==currentX&&Math.abs(parentY-currentY)==1)||
-                (parentY==currentY&&Math.abs(parentX-currentX)==1)
-            ){
-                path.unshift([parentX,parentY]);
-                currentIndex=[parentX,parentY];
-                break;
+    let queue=[[start]];
+    while(queue.length>0){
+        let path=queue.shift();
+        //console.log(path);
+        let currentIndex=path[path.length-1];
+        //console.log(currentIndex);
+        if(currentIndex[0]===target[0] && currentIndex[1]===target[1]){
+            return path;
+        }
+        let currentPossibleMoves=possibleMoves(currentIndex);
+        //console.log(currentPossibleMoves);
+        for(const move of currentPossibleMoves){
+            if(visitedIndexes.some(([x,y])=>x===move[0] && y===move[1])){
+                let newPath=[...path,move];
+                queue.push(newPath);
             }
         }
     }
-    return path;
+    return null;
 }
 
- let test=movesQueue([0,0],[3,3]);
-console.log(test);
 
-console.log(shortestPath(test.visited,test.start,test.target));
+/*
+let test=movesQueue([0,0],[3,3]);
+console.log(test);
+*/
+
+knightMoves([3,3],[4,3]);
